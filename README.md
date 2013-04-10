eb326093039326b639829dde0aabfa2f3688e6b3
Chris McCabe <chris@justni.com>
2013-04-10 21:34:09 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+
+A php answer to the spotify question.
+
+The Question:
+https://www.spotify.com/uk/jobs/tech/reversed-binary/
+
+The answer:
+https://github.com/adulion/spotify_binary_answer
+
+Note:
+The answer can not be written in php and has to run from the
+Command line but this just shows a simple way to achieve 
+An answer in php
+
+chris[at]justni[dot].com
--- spotify.php
+++ spotify.php
@@ -3,7 +3,16 @@
 /**
  *	A php answer to the spotify question.
  *	
- * https://www.spotify.com/uk/jobs/tech/reversed-binary/
+ *	The Question:
+ *  https://www.spotify.com/uk/jobs/tech/reversed-binary/
+ *
+ *  The answer:
+ *  https://github.com/adulion/spotify_binary_answer
+ *
+ *  Note:
+ *  The answer can not be written in php and has to run from the
+ *  Command line but this just shows a simple way to achieve 
+ *  An answer in php
  *
  *	chris[at]justni[dot].com
  * 
